73070d963eb7667c5425bb01d07c57d488bb7b7a
Laura Gao <lauragao86868@gmail.com>
2022-03-12 12:31:05 -0500

    Created `Rounds` data class which allows a `beginRounds` method and show rules when choosing a specific level

--- Wordle.java
+++ Wordle.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Wordle {
 
     interface VoidFunction {
+        // Dummy interface for typing of functions with no inputs and no outputs
         void run();
     }
 
@@ -27,27 +28,68 @@ public class Wordle {
         }
     }
 
+    final static class Round {
+        // Data class to store information about a round.
+        private final String name;
+        private final String description;
+        private final int numLetters;
+
+        public Round(String name, String description, int numLetters) {
+            this.name = name;
+            this.description = description;
+            this.numLetters = numLetters;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public int getNumLetters() {
+            return numLetters;
+        }
+    }
+
     // Text colors
-    public static final String RESET = "\033[0m"; // Text Reset
-    public static final String BLACK = "\033[0;30m"; // BLACK
-    public static final String RED = "\033[0;31m"; // RED
-    public static final String GREEN = "\033[1;92m"; // GREEN
-    public static final String ORANGE = "\033[1;38;2;255;165;0m"; // ORANGE
-    public static final String WHITE = "\033[1;97m"; // WHITE
+    public static final String RESET = "\033[0m"; // Text reset (clears all styles that have been applied)
+    public static final String BLACK = "\033[0;30m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[1;92m";
+    public static final String ORANGE = "\033[1;38;2;255;165;0m";
+    public static final String WHITE = "\033[1;97m";
+
+    // Each entry of the 2D array represents a round.
+    // Each entry of each nested array is [name of round, description of round,
+    // number of letters in this word's round]
+    private static final Round[] ROUNDS = {
+            new Round("CLASSIC WORDLE", "You will be given a 5-letter word and you must guess it within 6 tries.", 5),
+            new Round("4-LETTER WORDLE", "You have 6 tries to guess the word.", 4),
+            new Round("6-LETTER WORDLE", "You have 10 tries to guess the word.", 6),
+            new Round("DOUBLE WORDLE",
+                    "You have six tries to guess two 5-letter words.\nEach guess will be used on both words simultaneously.",
+                    5),
+    };
 
     public static void main(String[] args) throws InterruptedException {
         Scanner in = new Scanner(System.in);
 
         // Infinite while loop such that the program runs until the user quits
         while (true) {
-            int level = welcomeScreen(in);
+            int round = welcomeScreen(in);
             clearScreen();
-            System.out.println(level);
-            if (level == 0) {
+            System.out.println(round);
+            if (round == 0) {
                 int totalScore = 0;
+                beginRound(in, 1, false);
                 totalScore = round1(in, totalScore);
+                beginRound(in, 2, true);
                 totalScore = round2(in, totalScore);
+                beginRound(in, 3, true);
                 totalScore = round3(in, totalScore);
+                beginRound(in, 4, true);
                 totalScore = round4(in, totalScore);
 
                 // Save final score to leaderboard.
@@ -59,20 +101,61 @@ public class Wordle {
                 String name = in.nextLine();
                 saveScore(name, totalScore);
                 System.out.println();
-                System.out.println("Thank you for playing. Press enter to return to the main menu.");
+                System.out.println("Thank you for playing, " + name + "! Press enter to return to the main menu.");
                 in.nextLine();
             } else {
-                if (level == 1) {
+                beginRound(in, round, false);
+                if (round == 1) {
                     round1(in, 0);
-                } else if (level == 2) {
+                } else if (round == 2) {
                     round2(in, 0);
-                } else if (level == 3) {
+                } else if (round == 3) {
                     round3(in, 0);
                 } else {
                     round4(in, 0);
                 }
             }
         }
+    }
+
+    private static void beginRound(Scanner in, int roundIndex, boolean skipRules) {
+        Round round = ROUNDS[roundIndex - 1];
+        clearScreen();
+        System.out.println("ROUND " + roundIndex + ": " + round.getName());
+        System.out.println();
+        System.out.println(round.getDescription());
+        System.out.println();
+        if (!skipRules) {
+            System.out.println();
+            System.out.println("Press enter to continue...");
+            System.out.println("If you already know how to play Wordle, enter 'S' to skip the rules.");
+            System.out.println();
+            while (true) {
+                String input = in.nextLine();
+                if (input.toUpperCase().equals("S")) {
+                    break;
+                } else {
+                    // Print wordle rules
+                    System.out.println("RULES:");
+                    System.out
+                            .println("Type a " + round.getNumLetters()
+                                    + " letter word and press enter. Each letter will be highlighted either...");
+                    System.out.println(WHITE + "White: this letter does not exist in the word.");
+                    System.out.println(
+                            ORANGE + "Orange: this letter exists in the word but is not in the right location.");
+                    System.out.println(GREEN + "Green: this letter exists in the word and is in the right location.");
+                    System.out.println(RESET);
+                    System.out.println("Press enter to continue...");
+                    in.nextLine();
+                    break;
+                }
+            }
+            clearScreen();
+            System.out.println(
+                    "You can type 'RQ' to rage quit the current level and skip to the next level at any time.");
+        }
+        System.out.println("Enter your first guess:");
+        System.out.println();
     }
 
     private static void saveScore(String name, int score) {
@@ -89,11 +172,11 @@ public class Wordle {
 
     private static MyResult mainWordle(Scanner in, final String answer, ArrayList<String> allWords)
             throws InterruptedException {
-        // This method carries out the logic of the wordle game (the UI of the user
+        // This method carries out the logic of a single wordle game (the UI of the user
         // guessing.)
         boolean successful = false;
         ArrayList<String> allGuesses = new ArrayList<String>(); // Store all guesses in a variable so we can print
-                                                                // em all out later.
+                                                                // them all out later.
         while (true) {
             String guess = getGuess(in, answer.length(), allWords,
                     () -> allGuesses.forEach((prevGuess) -> {
@@ -127,12 +210,7 @@ public class Wordle {
 
     private static int round4(Scanner in, int totalScore) throws InterruptedException {
         // Double wordle
-        System.out.println("ROUND 4: DOUBLE WORDLE");
-        System.out.println();
-        System.out.println("You have six tries to guess two 5-letter words.");
-        System.out.println("Each guess will be used on both words simultaneously.");
-        System.out.println();
-        System.out.println("Enter your first guess:");
+        final String gap = "                   "; // Gap between the two wordles when they are printed out.
 
         final ArrayList<String> possibleAnswers = getWords("./wordlist_answers.txt");
         final ArrayList<String> allWords = getWords("./wordlist_all.txt");
@@ -148,75 +226,74 @@ public class Wordle {
         }
         ArrayList<String> allGuesses = new ArrayList<String>();
 
-        boolean answer1Solved = false;
-        boolean answer2Solved = false;
+        // These two variables how many guesses it takes to solve each answer
+        // and is 0 if the answer is unsolved.
+        int answer1Solved = 0;
+        int answer2Solved = 0;
 
         while (true) {
             VoidFunction printCurrentScreen = () -> {
                 clearScreen();
                 allGuesses.forEach(prevGuess -> {
                     printColoredWord(prevGuess, answer1, false);
-                    System.out.print("                   ");
+                    System.out.print(gap);
                     printColoredWord(prevGuess, answer2, false);
                     System.out.println();
                 });
             };
-            String guess = getGuess(in, 5, allWords, () -> {
-                clearScreen();
-                allGuesses.forEach(prevGuess -> {
-                    printColoredWord(prevGuess, answer1, false);
-                    System.out.print("                   ");
-                    printColoredWord(prevGuess, answer2, false);
-                    System.out.println();
-                });
-            });
+            String guess = getGuess(in, 5, allWords, printCurrentScreen);
             if (guess.equals("RQ")) {
                 System.out.println("You rage quit. The answers are: " + answer1 + " and " + answer2);
                 break;
             }
 
             clearScreen();
-            allGuesses.forEach(prevGuess -> {
-                printColoredWord(prevGuess, answer1, false);
-                System.out.print("                   ");
-                printColoredWord(prevGuess, answer2, false);
-                System.out.println();
-            });
-            printColoredWord(guess, answer1, true);
-            System.out.print("                   ");
-            printColoredWord(guess, answer2, true);
+            printCurrentScreen.run();
+            // If an ansewr has already been solved, do not print out this guess for the
+            // answer.
+            if (answer1Solved == 0) {
+                printColoredWord(guess, answer1, true);
+            } else {
+                System.out.println("     ");
+            }
+            System.out.print(gap);
+            if (answer2Solved == 0) {
+                printColoredWord(guess, answer2, true);
+            } else {
+                System.out.println("     ");
+            }
             System.out.println();
             allGuesses.add(guess);
 
             if (guess.equals(answer1))
-                answer1Solved = true;
+                answer1Solved = allGuesses.size();
             if (guess.equals(answer2))
-                answer2Solved = true;
-            if (answer1Solved && answer2Solved)
-                break;
+                answer2Solved = allGuesses.size();
+            if (answer1Solved > 0 && answer2Solved > 0)
+                break; // Exit when both answers are solved.
         }
 
-        int newPoints = (answer1Solved && allGuesses.size() <= 6) ? (int) Math.pow((7 - allGuesses.size()), 2) * 50
+        // Calculate score.
+        int newPoints = 0;
+        // Add points for each answer solved under the allowed number of guesses.
+        newPoints += (answer1Solved > 0 && answer1Solved <= 6) ? (int) Math.pow((7 - answer1Solved), 2) * 50
                 : 0;
-        newPoints += (answer2Solved && allGuesses.size() <= 6) ? (int) Math.pow((7 - allGuesses.size()), 2) * 50 : 0;
-        newPoints += (answer1Solved && answer2Solved && allGuesses.size() <= 6)
+        newPoints += (answer2Solved > 0 && answer2Solved <= 6) ? (int) Math.pow((7 - answer2Solved), 2) * 50
+                : 0;
+
+        // Add bonus points if both answers are solved under the allowed number of
+        // guesses.
+        newPoints += (answer1Solved > 0 && answer2Solved > 0 && allGuesses.size() <= 6)
                 ? (int) Math.pow((7 - allGuesses.size()), 2) * 100
                 : 0;
         totalScore += newPoints;
-        System.out.println("This earns you an additional " + newPoints + " points for a total score of " + totalScore);
-        System.out.println();
-        System.out.println("Press enter to continue...");
+        finishRound(newPoints, totalScore, in);
 
         return totalScore;
     }
 
     private static int round3(Scanner in, int totalScore) throws InterruptedException {
         // 6 letter words
-        clearScreen();
-        System.out.println("ROUND 3: 6 LETTER WORDLE");
-        System.out.println();
-        System.out.println("You have 10 tries to guess the word.");
-        System.out.println("Enter your first guess:");
 
         final ArrayList<String> words = getWords("./wordlist_6_all.txt");
         final String answer = words.get((int) (Math.random() * words.size()));
@@ -227,19 +304,12 @@ public class Wordle {
 
         int newPoints = (successful && allGuesses.size() <= 6) ? (int) Math.pow((7 - allGuesses.size()), 2) * 100 : 0;
         totalScore += newPoints;
-        System.out.println("This earns you an additional " + newPoints + " points for a total score of " + totalScore);
-        System.out.println();
-        System.out.println("Press enter to continue...");
-        in.nextLine();
-        clearScreen();
+        finishRound(newPoints, totalScore, in);
         return totalScore;
 
     }
 
     private static int round2(Scanner in, int totalScore) throws InterruptedException {
-        clearScreen();
-        System.out.println("ROUND 2: 4 LETTER WORDLE");
-        System.out.println("Enter your first guess:");
 
         final ArrayList<String> words_possible_ansnwers = getWords("./wordlist_4_answers.txt");
         final ArrayList<String> allWords = getWords("./wordlist_4_all.txt");
@@ -250,46 +320,12 @@ public class Wordle {
                 ? (int) Math.pow((7 - result.getFirst().size()), 2) * 100
                 : 0;
         totalScore += newPoints;
-        System.out.println("This earns you an additional " + newPoints + " points for a total score of " + totalScore);
-        System.out.println();
-        System.out.println("Press enter to continue...");
-        in.nextLine();
-        clearScreen();
+        finishRound(newPoints, totalScore, in);
         return totalScore;
 
     }
 
     private static int round1(Scanner in, int totalScore) throws InterruptedException {
-        clearScreen();
-        System.out.println("ROUND 1: CLASSIC WORDLE");
-        System.out.println("You will be given a 5-letter word and you must guess it within 6 tries.");
-        System.out.println();
-        System.out.println("Press enter to continue...");
-        System.out.println("If you already know how to play Wordle, enter 'S' to skip the rules.");
-        System.out.println();
-        while (true) {
-            String input = in.nextLine();
-            if (input.toUpperCase().equals("S")) {
-                break;
-            } else {
-                // Print wordle rules
-                System.out.println("RULES:");
-                System.out.println("Type a 5 letter word and press enter. Each letter will be highlighted either...");
-                System.out.println(WHITE + "White: this letter does not exist in the word.");
-                System.out.println(ORANGE + "Orange: this letter exists in the word but is not in the right location.");
-                System.out.println(GREEN + "Green: this letter exists in the word and is in the right location.");
-                System.out.println(RESET);
-                System.out.println("Press enter to continue...");
-                in.nextLine();
-                break;
-            }
-        }
-        clearScreen();
-        System.out.println(
-                "You can type 'RQ' to rage quit the current level and skip to the next level at any time.");
-        System.out.println("Enter your first guess:");
-        System.out.println();
-
         final ArrayList<String> possibleAnswers = getWords("./wordlist_answers.txt");
         final ArrayList<String> allWords = getWords("./wordlist_all.txt");
         final String answer = possibleAnswers.get((int) (Math.random() * possibleAnswers.size()));
@@ -300,16 +336,15 @@ public class Wordle {
 
         int newPoints = (successful && allGuesses.size() <= 6) ? (int) Math.pow((7 - allGuesses.size()), 2) * 100 : 0;
         totalScore += newPoints;
-        System.out.println("This earns you an additional " + newPoints + " points for a total score of " + totalScore);
-        System.out.println();
-        System.out.println("Press enter to continue...");
-        in.nextLine();
-        clearScreen();
+        finishRound(newPoints, totalScore, in);
         return totalScore;
     }
 
     private static void printColoredWord(String guess, final String answer, boolean showAnimation) {
-        // Note that this method does not print linebreak after the word.
+        // This method accepts a guess and the correct answer. It will print out the
+        // guess with each letter hightlighted with the appropriate color according to
+        // the rules of Wordle.
+        // Note that this method does not print a linebreak after the word.
         int wordLength = answer.length();
         for (int i = 0; i < wordLength; i++) {
             if (guess.charAt(i) == answer.charAt(i)) {
@@ -452,6 +487,7 @@ public class Wordle {
     }
 
     private static int roundsScreen(Scanner in) {
+        final String rounds = "1234";
         System.out.println("ROUNDS:");
         System.out.println("1 - Classic 5-letter Wordle");
         System.out.println("2 - 4-letter Wordle");
@@ -460,21 +496,21 @@ public class Wordle {
         System.out.println();
 
         System.out.println(
-                "By playing a specific level, you will not be able to accrue a total score and will not be on the leaderboard.");
-        System.out.println("This is for practicing a specific level to get better.");
+                "By playing a specific level, you will not be able to accrue a total score and will not be competing for a position on the leaderboard.");
+        System.out.println("This is for practicing a specific level that you want to improve or try out.");
         System.out.println();
         System.out.println("Enter the number of the round you want to jump to.");
-        System.out.println("Enter 'R' to return to the main menu.");
+        System.out.println("Or, enter 'R' to return to the main menu.");
         System.out.println();
-        String level = in.nextLine();
+        String round = in.nextLine();
         while (true) {
-            if (level.length() == 1 && "1234".indexOf(level) > -1) {
-                return Integer.parseInt(level);
-            } else if (level.toUpperCase().equals("R")) {
+            if (round.length() == 1 && rounds.indexOf(round) > -1) {
+                return Integer.parseInt(round);
+            } else if (round.toUpperCase().equals("R")) {
                 welcomeScreen(in);
             } else {
                 System.out.println("Invalid input. Please try again.");
-                level = in.nextLine();
+                round = in.nextLine();
             }
         }
     }
@@ -502,5 +538,13 @@ public class Wordle {
             System.exit(0);
         }
         return words;
+    }
+
+    private static void finishRound(int newPoints, int totalScore, Scanner in) {
+        System.out.println("This earns you an additional " + newPoints + " points for a total score of " + totalScore);
+        System.out.println();
+        System.out.println("Press enter to continue...");
+        in.nextLine();
+        clearScreen();
     }
 }
